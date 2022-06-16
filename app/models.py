24aea37a9937645e66cc55bd9be4f64a6d9135a8
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-16 21:08:26 +0300

    ft:model-class - adds a model class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    student_first_name = models.CharField(max_length=50)
+    student_last_name = models.CharField(max_length=50)
+    students_Age = models.IntegerField()
+    student_gender = models.CharField(max_length=30)
+    student_speciality = models.CharField(max_length=50)
+    student_laptop_type = models.CharField(max_length=50)
+    students_bio = models.TextField()
+    
